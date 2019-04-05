170ea5e3f18a7e0879de9b98ebf96ced6d2b776a
Lin Yuanhong <linyuanhong@G104E1900437.local>
2019-04-05 17:40:47 +0800

    watch 123

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,18 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './Header.css'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to='/aa'> aa </Link>
+      <Link to='/aa'> aa </Link>
+      <Link to='/aa'> aa </Link>
+      <Link to='/aa'> aa </Link>
+      <Link to='/buy'> <button>Log in</button> </Link>
     </div>
+    <div className="HeaderBackgroundcolor"> </div>
+    
   </div>
 )
 
