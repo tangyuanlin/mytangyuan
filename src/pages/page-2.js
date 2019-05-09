31746f48507a7b59bbfc69b54ed6a1fbdfcc88b6
Lin Yuanhong <linyuanhong@G104E1900437.local>
2019-05-09 16:01:46 +0800

    0509

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './SecondPage.css'
+import img1 from './WechatIMG203.jpeg'
+import video1 from './1557384396032894.mp4'
+
 
 const SecondPage = () => (
-  <div>
+  <div className="div1">
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+    <img className="img1" src={img1}></img>
+    <video src={video1}  controls="controls" autoplay="play" loop="loop"></video>
+
+   
   </div>
 )
 
