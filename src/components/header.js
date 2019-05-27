5dcf110cf94b23cc54f7dc121043a073d386fa08
Lin Yuanhong <linyuanhong@G104E1900437.local>
2019-05-27 13:42:58 +0800

    img1a

--- src/components/header.js
+++ src/components/header.js
@@ -5,11 +5,11 @@ import './Header.css'
 const Header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
-      <Link to='/aa'> aa </Link>
-      <Link to='/aa'> aa </Link>
-      <Link to='/aa'> aa </Link>
-      <Link to='/aa'> aa </Link>
-      <Link to='/buy'> <button>Log in</button> </Link>
+      {/* <Link to='/aa'>Tangyuan Design </Link> */}
+      {/* <Link to='/aa'> aa </Link> */}
+      {/* <Link to='/aa'> aa </Link> */}
+      <Link to='/'> Tangyuan Design </Link>
+      {/* <Link to='/buy'> <button>Log in</button> </Link> */}
     </div>
     <div className="HeaderBackgroundcolor"> </div>
     
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,19 +2,30 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './SecondPage.css'
 import img1 from './WechatIMG203.jpeg'
+import img1a from './WechatIMG204.jpg'
+import imgbgc from './1112.png'
+import gif1 from './dongxiao.gif'
 import video1 from './1557384396032894.mp4'
 
 
 const SecondPage = () => (
   <div className="div1">
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-    <img className="img1" src={img1}></img>
-    <video src={video1}  controls="controls" autoplay="play" loop="loop"></video>
+    <img className="imgbgc" src={imgbgc} ></img> 
+    <div className="div800">
+        <div className="div2">
+            <h1 className="page2h1">Hi from the second page</h1>
+            <p className="page2p">Welcome to page 2</p>
+            <Link to="/">Go back to the homepage</Link>
+            <img className="img1" data-src={img1a} src={img1}></img>
+            <img className="gif1" src={gif1}></img>
+            <video className="video1" src={video1}  controls="controls" autoplay="play" loop="loop"></video>
 
+        </div> */}
+
+    </div>
    
   </div>
 )
 
 export default SecondPage
+
