881745cd7b8238ffc992637a997198eaca8d28cd
Lin Yuanhong <linyuanhong@G104E1900437.local>
2019-05-28 08:49:52 +0800

    imga2

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,81 +1,81 @@
-// import React from 'react'
+import React from 'react'
 
-// const NotFoundPage = () => (
-//   <div>
-//     <h1>NOT FOUND</h1>
-//     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-//   </div>
-// )
+const NotFoundPage = () => (
+  <div>
+    <h1>NOT FOUND</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </div>
+)
 
-// export default NotFoundPage
-
-
+export default NotFoundPage
 
 
-import React, {Component} from 'react';
-import Camera from 'react-camera';
 
 
-export default class App extends Component {
+// import React, {Component} from 'react';
 
-    constructor(props) {
-      super(props);
-      this.takePicture = this.takePicture.bind(this);
-    }
+
+
+// export default class App extends Component {
+
+//     constructor(props) {
+//       super(props);
+//       this.takePicture = this.takePicture.bind(this);
+//     }
   
-    takePicture() {
-      this.camera.capture()
-      .then(blob => {
-        this.img.src = URL.createObjectURL(blob);
-        this.img.onload = () => { URL.revokeObjectURL(this.src); }
-      })
-    }
+//     takePicture() {
+//       this.camera.capture()
+//       .then(blob => {
+//         this.img.src = URL.createObjectURL(blob);
+//         this.img.onload = () => { URL.revokeObjectURL(this.src); }
+//       })
+//     }
   
-    render() {
-      return (
-        <div style={style.container}>
-          <Camera
-            style={style.preview}
-            ref={(cam) => {
-              this.camera = cam;
-            }}
-          >
-            <div style={style.captureContainer} onClick={this.takePicture}>
-              <div style={style.captureButton} />
-            </div>
-          </Camera>
-          <img
-            style={style.captureImage}
-            ref={(img) => {
-              this.img = img;
-            }}
-          />
-        </div>
-      );
-    }
-  }
+//     render() {
+//       return (
+//         <div style={style.container}>
+//           <Camera
+//             style={style.preview}
+//             ref={(cam) => {
+//               this.camera = cam;
+//             }}
+//           >
+//             <div style={style.captureContainer} onClick={this.takePicture}>
+//               <div style={style.captureButton} />
+//             </div>
+//           </Camera>
+//           <img
+//             style={style.captureImage}
+//             ref={(img) => {
+//               this.img = img;
+//             }}
+//           />
+//         </div>
+//       );
+//     }
+//   }
   
-  const style = {
-    preview: {
-      position: 'relative',
-    },
-    captureContainer: {
-      display: 'flex',
-      position: 'absolute',
-      justifyContent: 'center',
-      zIndex: 1,
-      bottom: 0,
-      width: '100%'
-    },
-    captureButton: {
-      backgroundColor: '#fff',
-      borderRadius: '50%',
-      height: 56,
-      width: 56,
-      color: '#000',
-      margin: 20
-    },
-    captureImage: {
-      width: '100%',
-    }
-  };
+//   const style = {
+//     preview: {
+//       position: 'relative',
+//     },
+//     captureContainer: {
+//       display: 'flex',
+//       position: 'absolute',
+//       justifyContent: 'center',
+//       zIndex: 1,
+//       bottom: 0,
+//       width: '100%'
+//     },
+//     captureButton: {
+//       backgroundColor: '#fff',
+//       borderRadius: '50%',
+//       height: 56,
+//       width: 56,
+//       color: '#000',
+//       margin: 20
+//     },
+//     captureImage: {
+//       width: '100%',
+//     }
+//   };
